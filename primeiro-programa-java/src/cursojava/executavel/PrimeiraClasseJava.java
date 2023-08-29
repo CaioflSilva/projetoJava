f07c8cecfdeda333588da615567b4cf4684328a3
Caio <Caio@DESKTOP-SQMMQ9B.Dlink>
2023-08-29 20:12:41 -0300

    tipo String de caracteres

--- primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -6,16 +6,16 @@ public class PrimeiraClasseJava {
 	/*main é um metodo auto executavel em java*/
 	public static void main(String[] args) {
 		
-	//char representa uma letra
+   //String no java sempre sera um texto de tamanho qualquer
 		
-	char pessoaFeminino = 'f';
-	
-	if (pessoaFeminino == 'f') {
-		System.out.println("Feminino");
-	}else {
-		System.out.println("Masculino");
-	}
-		
+String textoQualquerTamanhoDeCaracter ="lmzxmcm.,zxmc,m.cxzm.,3356656565asd6as5d" ;
+		               //independentemente de quqlquer tamanho ou qualquer caracterees
+
+String mome;
+String cpf;
+String telefone;
+String endereco;
+
 		
 	} 
  

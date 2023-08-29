7ae14c936c69895c225409391154d15d15a22683
Caio <Caio@DESKTOP-SQMMQ9B.Dlink>
2023-08-28 22:14:25 -0300

    Fazendo operaçoes entre numeros inteiros

--- primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -2,24 +2,21 @@ package cursojava.executavel;
 
 public class PrimeiraClasseJava {
 	
-	//variavel global é acessivel a todos e o seu valor é compartilhado
-	static int maiorIdadeGlobal = 30;
-	
-	
+		
 	/*main é um metodo auto executavel em java*/
 	public static void main(String[] args) {
-	
-	//variavel local porque pertence somente a esse metodo
 		
-	int maiorIdade = 18;
-	System.out.println("valor variavel local = " + maiorIdadeGlobal);
-	System.out.println("valor variavel local = " + maiorIdade);
-		metodo2();
+	int nota1 = 90;
+    int nota2 = 70;
+    int nota3 = 50;
+    int nota4 = 88;
+
+    System.out.println("Media dos Alunos = " + (nota1 + nota2 + nota3 + nota4) /4);
+    
 	}
  
-     public static void metodo2 () {
-    	System.out.println("valor da maior idade global = " + maiorIdadeGlobal); 
+     
      }
 	
 	
-}
+

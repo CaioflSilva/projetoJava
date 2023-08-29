1da79850f05a220bea8803426dc6c046b11cd9d6
Caio <Caio@DESKTOP-SQMMQ9B.Dlink>
2023-08-29 20:28:14 -0300

    concatenaçao de variaveis

--- primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -6,17 +6,18 @@ public class PrimeiraClasseJava {
 	/*main é um metodo auto executavel em java*/
 	public static void main(String[] args) {
 		
-   //String no java sempre sera um texto de tamanho qualquer
+   //Concatenaçao = unir ou juntar dados
 		
-String textoQualquerTamanhoDeCaracter ="lmzxmcm.,zxmc,m.cxzm.,3356656565asd6as5d" ;
-		               //independentemente de quqlquer tamanho ou qualquer caracterees
-
-String mome;
-String cpf;
-String telefone;
-String endereco;
-
 		
+String nome = "Caio Filipe";
+String cpf = "08536597408";
+String telefone = "81 86625026";
+String endereco = "Recife -Pe";
+      
+
+String saida =   "Meu Nome é: " + nome + " Do Cpf: " + cpf + " Meu Cel é : " + telefone + " E moro em : " + endereco ;
+   System.out.println(saida);
+	
 	} 
  
      

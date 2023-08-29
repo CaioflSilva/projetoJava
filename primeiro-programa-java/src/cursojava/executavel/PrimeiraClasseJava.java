579433f311798a0987236dc28a9b77db1dd21769
Caio <Caio@DESKTOP-SQMMQ9B.Dlink>
2023-08-29 20:05:24 -0300

    tipo Char de caracteres

--- primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -6,18 +6,21 @@ public class PrimeiraClasseJava {
 	/*main é um metodo auto executavel em java*/
 	public static void main(String[] args) {
 		
-	double nota1 = 90;
-	double nota2 = 70;
-    double nota3 = 50;
-    double nota4 = 88;
-
-     double mediafinal =  (nota1 + nota2 + nota3 + nota4) /4;
-    
-     System.out.println("Media das Notas = " + mediafinal);
+	//char representa uma letra
+		
+	char pessoaFeminino = 'f';
+	
+	if (pessoaFeminino == 'f') {
+		System.out.println("Feminino");
+	}else {
+		System.out.println("Masculino");
+	}
+		
+		
 	} 
  
      
-     }
+ }
 	
 	
 

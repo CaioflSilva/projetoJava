9b661298f7690204db95c992cd7ac52e200bd977
Caio <Caio@DESKTOP-SQMMQ9B.Dlink>
2023-08-29 21:29:26 -0300

    operacoes logicas em if e else

--- primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -1,27 +1,27 @@
 package cursojava.executavel;
 
 public class PrimeiraClasseJava {
-	
-		
-	/*main é um metodo auto executavel em java*/
+
+	/* main é um metodo auto executavel em java */
 	public static void main(String[] args) {
-		
-   //Concatenaçao = unir ou juntar dados
-		
-		
-String nome = "Caio Filipe";
-String cpf = "08536597408";
-String telefone = "81 86625026";
-String endereco = "Recife -Pe";
-      
 
-String saida =   "Meu Nome é: " + nome + " Do Cpf: " + cpf + " Meu Cel é : " + telefone + " E moro em : " + endereco ;
-   System.out.println(saida);
-	
-	} 
- 
-     
- }
-	
-	
+		int nota1 = 90;
+		int nota2 = 60;
+		int nota3 = 90;
+		int nota4 = 90;
+		int media = 0;
 
+		media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+		if (media >= 70) {
+			System.out.println("Aluno Aprovado: " + media);
+
+		} else if (media >= 40 && media <= 69) {
+			System.out.println("Aluno em recuperacao:" + media);
+		} else {
+			System.out.println("Aluno Reprovado: " + media);
+		}
+
+	}
+
+}

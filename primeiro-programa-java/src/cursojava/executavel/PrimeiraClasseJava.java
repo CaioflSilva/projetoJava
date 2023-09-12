0fac17882951453e44b57e58619e634a79a25cc8
Caio <Caio@DESKTOP-SQMMQ9B.Dlink>
2023-09-11 22:43:58 -0300

    Usando estruturas de repetiçao FOR com ( NUMERO ++ E NUMERO --)

--- primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -5,22 +5,10 @@ public class PrimeiraClasseJava {
 	/* main é um metodo auto executavel em java */
 	public static void main(String[] args) {
 
-		// estrutura de repetiçao while
-
-		int numero = 0;
-
-		while (numero <= 60) { // verifica e depois execulta o codigo
-			System.out.println("O numero atual é : " + numero);
-			numero++;
+		// estrutura de repetiçao FOR
+		for (int numero = 10; numero >= 0; numero--) {
+System.out.println("Numero atual é : " + numero);
 		}
-
-		//
-		int numero2 = 0;
-		do { // primeiro ele execulta e depois verifica
-
-			System.out.println("O numero atual é : " + numero2);
-			numero2++;
-		} while (numero2 <= 60);
 
 	}
 }

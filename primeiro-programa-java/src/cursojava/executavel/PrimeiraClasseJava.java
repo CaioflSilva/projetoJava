ccde940b93831a531e44c9cfc40f69aed8ef6393
Caio <Caio@DESKTOP-SQMMQ9B.Dlink>
2023-09-12 22:34:47 -0300

    Criando entrada de dados com Opção de confirmação

--- primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -7,19 +7,28 @@ public class PrimeiraClasseJava {
 	/* main é um metodo auto executavel em java */
 	public static void main(String[] args) {
 
-	String carros = JOptionPane.showInputDialog("Informe a quantidade de carros?");
-	String pessoas = JOptionPane.showInputDialog("Informe a quantidade de pessoas?");
+		String carros = JOptionPane.showInputDialog("Informe a quantidade de carros?");
+		String pessoas = JOptionPane.showInputDialog("Informe a quantidade de pessoas?");
 
-	double carroNumero = Double.parseDouble(carros);
-	double pessoaNumero =Double.parseDouble(pessoas);
-	
-	int divisao = (int)(carroNumero / pessoaNumero);
-	
-	double resto = carroNumero % pessoaNumero;
-	
-JOptionPane.showMessageDialog(null, "Divisão para pessoas deu " + divisao + " Carros e sobrou " + resto + " carros" );
+		double carroNumero = Double.parseDouble(carros);
+		double pessoaNumero = Double.parseDouble(pessoas);
 
-	
+		int divisao = (int) (carroNumero / pessoaNumero);
+
+		double resto = carroNumero % pessoaNumero;
+
+		int resposta = JOptionPane.showConfirmDialog(null, "Deseja ver o resultado da divisão ?");
+
+		if (resposta == 0) {
+			JOptionPane.showMessageDialog(null,
+					"Divisão para pessoas deu " + divisao + " Carros e sobrou " + resto + " carros");
+		}
+
+		resposta = JOptionPane.showConfirmDialog(null, "Deseja ver o Resto da divisão ?");
+		if (resposta == 0) {
+			JOptionPane.showConfirmDialog(null, "O resto da é " + resto);
+		}
+
 	}
 
 }

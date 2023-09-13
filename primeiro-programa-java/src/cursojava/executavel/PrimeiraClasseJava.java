068ca5dac471874874e91ff8f8a39f3dc5271272
Caio <Caio@DESKTOP-SQMMQ9B.Dlink>
2023-09-12 22:10:43 -0300

    usando o comando JoptionPane.showMessageDialog e usando entrada e saida de dados .

--- primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -1,19 +1,25 @@
 package cursojava.executavel;
 
+import javax.swing.JOptionPane;
+
 public class PrimeiraClasseJava {
 
 	/* main é um metodo auto executavel em java */
 	public static void main(String[] args) {
 
-		//resto da divisao %
-		
-		double carro = 9;
-		double pessoa = 2;
-		
-		double resto = carro % pessoa;
-				System.out.println("Sobraram Exatamente : " + resto + " Carros. ");
-		
-		
+	String carros = JOptionPane.showInputDialog("Informe a quantidade de carros?");
+	String pessoas = JOptionPane.showInputDialog("Informe a quantidade de pessoas?");
+
+	double carroNumero = Double.parseDouble(carros);
+	double pessoaNumero =Double.parseDouble(pessoas);
+	
+	int divisao = (int)(carroNumero / pessoaNumero);
+	
+	double resto = carroNumero % pessoaNumero;
+	
+JOptionPane.showMessageDialog(null, "Divisão para pessoas deu " + divisao + " Carros e sobrou " + resto + " carros" );
+
+	
 	}
 
 }

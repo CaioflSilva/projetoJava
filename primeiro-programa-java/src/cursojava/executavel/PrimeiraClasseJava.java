e910e8efc01db4477d77f2486a83c98ce7b62fcc
Caio <Caio@DESKTOP-SQMMQ9B.Dlink>
2023-09-12 21:42:14 -0300

    usando divisões e resto com mod do java

--- primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -5,17 +5,15 @@ public class PrimeiraClasseJava {
 	/* main é um metodo auto executavel em java */
 	public static void main(String[] args) {
 
-		// estrutura de repetiçao FOR e Continue
-
-		for (int numero = 0; numero <= 10; numero++) {
-			if (numero == 3 || numero == 6 || numero == 9) {
-				System.out.println("Obaaah, encontrei o numero " + numero);
-				continue;
-			}
-
-			System.out.println("processando laço de repetição");
-		}
-
+		//resto da divisao %
+		
+		double carro = 9;
+		double pessoa = 2;
+		
+		double resto = carro % pessoa;
+				System.out.println("Sobraram Exatamente : " + resto + " Carros. ");
+		
+		
 	}
 
 }

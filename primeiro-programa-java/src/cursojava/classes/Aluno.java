38c2e68bcc587be8c06d14e93058e0206d0777cb
Caio <Caio@DESKTOP-SQMMQ9B.Dlink>
2023-09-18 20:21:43 -0300

    Criando A primeira classe e obejto Aluno.

---
+++ primeiro-programa-java/src/cursojava/classes/Aluno.java
@@ -0,0 +1,17 @@
+package cursojava.classes;
+
+//Essa é nosaa classe/Objeto que representa o Aluno
+public class Aluno {
+
+	String nome;
+	int idade;
+	String dataNascimento;
+	String RegistroGeral;
+	String numeroCpf;
+	String nomeMae;
+	String nomePai;
+	String dataMatricula;
+	String nomeEscola;
+	String serieMatriculado;
+
+}
